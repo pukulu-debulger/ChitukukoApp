2f0d5ea8ed569086b700626cec0906048b2882dd
Pre-pukulu <bed-com-50-21@unima.ac.mw>
2026-01-20 14:38:36 +0200

    configuring tailwind.css

--- malawi-agri-platform/src/App.jsx
+++ malawi-agri-platform/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className='bg-blue-200 p-4 text-center flex flex-col items-center min-h-screen'>
+      <div className='bg-blue-200 p-4 text-center'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
